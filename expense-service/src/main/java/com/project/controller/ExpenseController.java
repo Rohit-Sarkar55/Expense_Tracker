85aa7dc71f77279fcdb3e7da0fb9d125a817d539
Das <anusrdas@deloitte.com>
2024-02-08 15:04:22 +0530

    mistake

--- expense-service/src/main/java/com/project/controller/ExpenseController.java
+++ expense-service/src/main/java/com/project/controller/ExpenseController.java
@@ -72,19 +72,19 @@ public class ExpenseController {
 //		return expenseService.viewAll();
 //		
 //	}
-	@GetMapping("")
-	public ResponseEntity<List<Expense>> viewAllExpense()
-	{
-		List<Expense> expense4=expenseService.viewAll();
-		if(!expense4.isEmpty())
-		{
-			return ResponseEntity.status(HttpStatus.OK).body(expense4);
-		}
-		else
-		{
-			return ResponseEntity.noContent().build();
-		}
-	}
+//	@GetMapping("")
+//	public ResponseEntity<List<Expense>> viewAllExpense()
+//	{
+//		List<Expense> expense4=expenseService.viewAll();
+//		if(!expense4.isEmpty())
+//		{
+//			return ResponseEntity.status(HttpStatus.OK).body(expense4);
+//		}
+//		else
+//		{
+//			return ResponseEntity.noContent().build();
+//		}
+//	}
 
 	@PutMapping("update")
 	public ResponseEntity<?> update(@RequestBody Expense expense)

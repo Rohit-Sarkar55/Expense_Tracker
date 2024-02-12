973a4958a9ddfdd93732d06dc4efc409326051ac
rohitsarkar <sarkarrohit650@gmail.com>
2024-02-12 11:27:17 +0530

    Navbar functions started

--- expense-tracker-frontend/src/App.js
+++ expense-tracker-frontend/src/App.js
@@ -11,8 +11,7 @@ function App() {
       <Navbar />
       
       <div className="content">
-       <Home /> 
-       <About />
+       
       </div>
     </div>
   );
--- expense-tracker-frontend/src/Navbar.js
+++ expense-tracker-frontend/src/Navbar.js
@@ -1,15 +1,43 @@
 import React, { useState } from 'react';
 import { Link } from 'react-scroll'; // If using React Router for navigation
 
+import About from './Components/About/About';
+import Home from './Home';
+
+
+
 const Navbar = () => {
   const [activeTab, setActiveTab] = useState('Home');
 
+  
   const handleTabClick = (tab) => {
-    setActiveTab(tab);
+    setActiveTab(tab)
+    console.log("handle click" + activeTab);
     // You can add additional logic here if needed
+    // renderComponent
+    
+  };
+
+
+  const renderComponent = () => {
+    console.log(activeTab);
+    switch (activeTab) {
+      case 'Home':
+        console.log("Home Triggered");
+        return <Home />
+      case 'Expenses':
+        // return <Expenses />;
+        return null;
+      case 'About':
+        return <About />;
+        
+      default:
+        return null; // Render null or a default component if no match
+    }
   };
 
   return (
+    <>
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className={`nav-item ${activeTab === 'Home' ? 'active' : ''}`} onClick={() => handleTabClick('Home')}>
@@ -22,7 +50,12 @@ const Navbar = () => {
           About
         </Link>
       </div>
+      
     </nav>
+    <div>
+    {renderComponent()}
+  </div>
+  </>
   );
 };
 
